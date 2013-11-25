a6f5b4cfb691564ada872fdedb5a11e948f67f14
Dejan Pantic <dpantic@gmail.com>
2013-11-26 00:02:47 +0100

    Finish layout and routes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
@@ -39,4 +43,11 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  config.include Capybara::DSL
+ end
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
 end
